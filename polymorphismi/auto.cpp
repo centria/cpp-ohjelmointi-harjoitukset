f57c46edadbfab4da1e87ed18ad67e174912bc85
jariisohanni <jari.isohanni@gmail.com>
2020-06-21 18:18:00 +0300

    Create auto.cpp

---
+++ polymorphismi/auto.cpp
@@ -0,0 +1,56 @@
+#include "auto.h"
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+//Määritellään staattinen muuttuja
+int Auto::tehdytAutot = 0;
+
+
+Auto::Auto()
+{
+    srand( (unsigned)time(NULL) );
+    tunniste  = rand();
+    tehdytAutot++;
+}
+
+Auto::Auto(long tunniste)
+{
+    this->tunniste  = tunniste;
+    tehdytAutot++;
+}
+
+void Auto::setNopeus(int n)
+{
+    this->nopeus = n;
+}
+
+void Auto::setTunniste(long t)
+{
+    this->tunniste = t;
+}
+
+int Auto::getNopeus()
+{
+    return nopeus;
+}
+
+long Auto::getTunniste()
+{
+    return  tunniste;
+}
+
+void Auto::kuvaile()
+{
+    cout << "Autossa on neljä rengasta" << endl;
+}
+
+void Auto::aanimerkki()
+{
+    cout << "geneerinen Auto ei anna äänimerkkiä" << endl;
+}
+
