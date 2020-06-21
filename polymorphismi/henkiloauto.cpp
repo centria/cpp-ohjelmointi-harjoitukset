054becf518171fa4cf47c50de7052db0924f6ed3
jariisohanni <jari.isohanni@gmail.com>
2020-06-21 18:17:48 +0300

    Create henkiloauto.cpp

---
+++ polymorphismi/henkiloauto.cpp
@@ -0,0 +1,23 @@
+#include "henkiloauto.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+HenkiloAuto::HenkiloAuto(): Auto()
+{
+
+}
+
+void HenkiloAuto::aanimerkki()
+{
+    cout << "tööt tööt" << endl;
+}
+
+
+void HenkiloAuto::kaynnista()
+{
+
+}
