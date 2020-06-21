563513832595f8aae99f31d7a074906f322c3cf8
jariisohanni <jari.isohanni@gmail.com>
2020-06-21 18:17:32 +0300

    Create henkiloauto.h

---
+++ polymorphismi/henkiloauto.h
@@ -0,0 +1,15 @@
+#ifndef HENKILOAUTO_H
+#define HENKILOAUTO_H
+
+#include "auto.h"
+
+class HenkiloAuto: public Auto
+{
+public:
+    HenkiloAuto();
+
+      void aanimerkki();
+      void kaynnista();
+};
+
+#endif // HENKILOAUTO_H
